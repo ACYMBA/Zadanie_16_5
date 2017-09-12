665d08de4e1cb9d8edb23785b73c03915f51f0be
albert <albert.cymbaluk@gmail.com>
2017-09-12 23:10:02 +0200

    new

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+[{
+id: 1,
+    text: 'clean room'
+}, {
+id: 2,
+    text: 'wash the dishes'
+}, {
+id: 3,
+    text: 'feed my cat'
+}]
+  </ul>
+
+export default TodoList
